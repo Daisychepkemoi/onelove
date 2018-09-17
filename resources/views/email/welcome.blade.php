d216b843e26d9ceff3ec8f71340bc0dab9c149b7
Daisychepkemoi <dchepkemoi408@gmail.com>
2018-09-17 17:41:31 +0300

    still a change in url
    still a change n url

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -7,7 +7,7 @@
 	
 	<h1>	Welcome  to One Love  {{$user->name}} </h1>
 	<h3>Please click the button below to activate your account </h3>
-	<button class="btn btn-success"><a href="http://localhost:8000/activate/{{$user->id}}">Activate Account</a></button>
+	<button class="btn btn-success"><a href="http://oneloveproposal.herokuapp.com/activate/{{$user->id}}">Activate Account</a></button>
 	<p>	Thank you for registering with us.</p>
 	
 </body>
