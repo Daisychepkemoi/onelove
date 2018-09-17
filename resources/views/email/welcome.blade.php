d1f84e9fac477eaaa1f40b549a1bf4c4bf562a08
Daisychepkemoi <dchepkemoi408@gmail.com>
2018-09-17 17:33:21 +0300

    updated url
    updated url

--- resources/views/email/welcome.blade.php
+++ resources/views/email/welcome.blade.php
@@ -5,10 +5,10 @@
 </head>
 <body>
 	
-	<h1>welcome  to One Love  {{$user->name}} </h1>
-	<h3>Please click the burron below to activate your account </h3>
-	<button class="btn btn-primary"><a href="/activate/{{$user->id}}">Activate Account</a></button>
-	<p>	Thank tou for registering with us.</p>
+	<h1>	Welcome  to One Love  {{$user->name}} </h1>
+	<h3>Please click the button below to activate your account </h3>
+	<button class="btn btn-success"><a href="http://localhost:8000/activate/{{$user->id}}">Activate Account</a></button>
+	<p>	Thank you for registering with us.</p>
 	
 </body>
 </html>
