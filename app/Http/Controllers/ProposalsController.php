4e1b448468516c5d4ed4987ef862868d194ea242
Daisychepkemoi <dchepkemoi408@gmail.com>
2018-09-17 18:39:15 +0300

    error correction
    errror corection

--- app/Http/Controllers/ProposalsController.php
+++ app/Http/Controllers/ProposalsController.php
@@ -17,7 +17,7 @@ class ProposalsController extends Controller
     {
         $this->middleware('auth')->except(['index','activate','activatepage']);
     }
-    
+
     public function index()
     {
         return view('proposal.index');
@@ -25,7 +25,7 @@ class ProposalsController extends Controller
     public function create()
     {
         $users=auth()->user();
-        
+
         return view('proposal.proposal', compact('users'));
     }
 
@@ -47,7 +47,7 @@ class ProposalsController extends Controller
 
         ]
     );
-        
+
         
         $draft= Proposal::create([
             'title'=>request('title'),
@@ -60,15 +60,15 @@ class ProposalsController extends Controller
             'background'=>request('background'),
             'activities'=>request('activities'),
             'budget'=>request('budget')
-            
+
         ]);
 
         switch ($request->input('publish')) {
             case 'save':
             $id=$draft->id;
-            
+
             // $users=DB::table('Users')->where('is_admin', true)->first();
-            $proposal=DB::table('Proposals')->where('id', $id)->first();
+            // $proposal=DB::table('proposals')->where('id', $id)->first();
             // \Notification::send($users, new adminnotify($proposal));
                         // dd($draft->id);
             return redirect()->to('/submitproposal/'.$id)->with('success', 'Proposal Submitted Successfully');
@@ -76,19 +76,19 @@ class ProposalsController extends Controller
 
             case 'draft':
             $id=$draft->id;
-            
+
             return redirect()->to('/draft/'.$id);
             break;
 
-            
+
         }
     }
     public function view(proposal $proposal)
     {
         $users=auth()->user();
-        $proposal = Proposal::latest()->where('Submitted_by', $users->email)->OrderBy('updated_at', 'desc')->get();
+        $proposal = Proposal::latest()->where('submitted_by', $users->email)->OrderBy('updated_at', 'desc')->get();
         $proposa=Proposal::where('draft', $proposal->draft=false)->orderBy('updated_at', 'desc')->get();
-        
+
         if ($proposal->count()>0) {
             return view('user.user', compact('proposal', 'users', 'proposa'));
         } else {
@@ -108,7 +108,7 @@ class ProposalsController extends Controller
     public function finalsubmit(request $request, $id)
     {
         $pro=DB::table('proposals')->where('id', $request->id)->update(['draft' => false]);
-        
+
         return redirect('/userproposal');
     }
 
@@ -118,18 +118,18 @@ class ProposalsController extends Controller
         $pro=DB::table('proposals')->where('id', $request->id)->update(['draft' => true]);
         $proposal=Proposal::where('id', $request->id)->first();
         
-        
+
         return redirect('/userproposal')->with('success', 'Draft saved successfully');
     }
     public function destroy(request $request, $id)
     {
         $users=auth()->user();
-        
+
         $pro=DB::table('proposals')->where('id', $request->id)->delete();
         
         return redirect('/userproposal')->with('success', 'Draft deleted successfully');
     }
-    
+
     public function update(request $request)
     {
         $proposal = Proposal::where('id', $request->id)->first();
@@ -145,17 +145,17 @@ class ProposalsController extends Controller
         $proposal->budget=$request->get('budget');
 
         $proposal->save();
-        
+
         $id=$proposal->id;
         switch ($request->input('save')) {
             case 'savedraft':
-            
-            
+
+
             return redirect()->to('/submitproposal/'.$id)->with('success', 'Proposal Submitted Successfully');
             break;
 
             case 'editdraft':
-            
+
             return redirect()->to('/draft/'.$id)->with('success', 'saved as draft Successfully');
             break;
             case 'deletedraft':
@@ -181,25 +181,25 @@ class ProposalsController extends Controller
         $users=auth()->user();
         $user=$users->email;
         $proposal = Proposal::where('submitted_by', $user)->where('stage', 'reject')->orderBy('updated_at','desc')->get();
-        
+
         return view('user.user',compact('proposal','users'));
     }
     public function stageoneuser(Proposal $proposal)
     {
         $users=auth()->user();
         $user=$users->email;
-        
+
         $proposal=Proposal::where('submitted_by', $user)->where('stage','stageone')->orderBy('updated_at','desc')->get();
-        
+
         return view('user.user',compact('proposal','users'));
     }
     public function userdrafts(Proposal $proposal)
     {
         $users=auth()->user();
         $user=$users->email;
-        
+
         $proposal=Proposal::where('submitted_by', $user)->where('draft',1)->orderBy('updated_at','desc')->get();
-        
+
         return view('user.user',compact('proposal','users'));
     }
     public function stagetwouser(Proposal $proposal)
@@ -214,7 +214,7 @@ class ProposalsController extends Controller
         $users=auth()->user();
         $user=$users->email;
         $proposal=Proposal::where('submitted_by', $user)->whereNull('stage')->orderBy('updated_at','desc')->get();
-        
+
         return view('user.user',compact('proposal','users'));
     }
     public function accepteduser(Proposal $proposal)
@@ -223,7 +223,7 @@ class ProposalsController extends Controller
         $users=auth()->user();
         $user=$users->email;
         $proposal=Proposal::where('submitted_by', $user)->where('stage','Accepted')->orderBy('updated_at','desc')->get();
-        
+
         return view('user.user',compact('proposal','users'));
     }
     public function userback()
